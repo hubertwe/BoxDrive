0976b743466267e86e0e098a11a9aaaec3d5928a
radx64 <radekteam@gmail.com>
2015-04-08 21:21:49 +0200

    Small fixes of remote

--- remote.py
+++ remote.py
@@ -1,25 +1,20 @@
-# coding: utf-8
-
 from __future__ import print_function, unicode_literals
 import os
 from boxsdk import Client
-from boxsdk.exception import BoxAPIException
-from boxsdk.object.collaboration import CollaborationRole
 from auth import authenticate
 
 def get_events(client):
     print(client.events().get_events(limit=100, stream_position='now'))
 
-
 def get_latest_stream_position(client):
     print(client.events().get_latest_stream_position())
 
 def run_examples(oauth):
-
     client = Client(oauth)
     get_events(client)
     get_latest_stream_position(client)
 
+    #routine below should be run on different thread cause it's blocking!
     for event in client.events().generate_events_with_long_polling():
         print('------------------------------------------------')
         print('New notification arrived!')
@@ -39,8 +34,6 @@ def run_examples(oauth):
         print('------------------------------------------------')
 
 def main():
-
-    # Please notice that you need to put in your client id and client secret in demo/auth.py in order to make this work.
     oauth = authenticate()
     run_examples(oauth)
     os._exit(0)

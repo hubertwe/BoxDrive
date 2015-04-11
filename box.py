4f26acf128a2f265c2edce585d3c7ca4c22a6ded
radx64 <radekteam@gmail.com>
2015-04-11 20:58:47 +0200

    Non blocking events polling. You have to distinct events by event_id cause they can be retransmitted few time as spec says.

--- box.py
+++ box.py
@@ -1,12 +1,22 @@
+# https://developers.box.com/docs/
+
 from __future__ import print_function, unicode_literals
 import os
 from boxsdk import Client
 from auth import authenticate
+import time
 
 class Box(Client):
     def __init__(self):
         self.auth = authenticate()
+        self.lastEventStreamPosition = 'now'
         Client.__init__(self, self.auth)
+
+    def getLastEvents(self):
+        eventsPack = self.events().get_events(limit=100, stream_position=self.lastEventStreamPosition)
+        self.lastEventStreamPosition = eventsPack['next_stream_position']
+        return eventsPack['entries']
+
 
 class Uploader:
     def __init__(self, box):
@@ -18,23 +28,10 @@ class Downloader:
 
 def main():
     box = Box()
-    #streamPosition = box.events().get_latest_stream_position()
-    #print(streamPosition)
-    #options = box.events().get_long_poll_options()
-    #while True:
-        #print(box.events().long_poll(options=options,stream_position=streamPosition))
-
-    streamPosition = 0
-
     while True:
-        returnInfo = box.events().get_events(limit=1, stream_position=streamPosition)
-        streamPosition = returnInfo['next_stream_position']
-        print(streamPosition)
-        for event in returnInfo['entries']:
-            pass #print(event)
-
-    #print(box.events().get_events(limit=2))
-
+        for event in box.getLastEvents():
+            print(event)
+        time.sleep(5)
 
     os._exit(0)
 

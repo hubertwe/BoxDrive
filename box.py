c08bfb8f1b21477ad6bb456f91cfa73ad4543130
radx64 <radek-team@wp.pl>
2015-04-15 23:48:12 +0200

    Internal Event type support. Box now filters duplicated events.

--- box.py
+++ box.py
@@ -4,20 +4,105 @@ from __future__ import print_function, unicode_literals
 import os
 import boxsdk
 from auth import authenticate
-#import time
+import time
+
+class EventType:
+    UNKNOWN = 0
+    CREATE = 1
+    UPDATE = 2
+    REMOVE = 3
+
+class Event:
+    def __init__(self):
+        self.type = EventType.UNKNOWN
+        self.is_directory = bool()
+        self.path = str()
+        self.sha1 = ""
 
 class Box(boxsdk.Client):
-
     def __init__(self):
         self.auth = authenticate()
         self.lastEventStreamPosition = 'now'
         boxsdk.Client.__init__(self, self.auth)
 
+    def __getEventType(self, boxEvent):
+        conversion = {'ITEM_CREATE' : EventType.CREATE,
+                        'ITEM_UPLOAD' : EventType.UPDATE,
+                        'ITEM_TRASH' : EventType.REMOVE}
+        return (conversion.get(boxEvent['event_type'], EventType.UNKNOWN))
+
+
+    def __getSource(self,boxEvent):
+        source = boxEvent.get('source')
+        if(source):
+            return source
+        else:
+            raise ValueError('Cant find event source')
+
+    def __getType(self, boxEvent):
+        source = self.__getSource(boxEvent)
+        typeName = source.get('type')
+        if(typeName):
+            return typeName
+        else:
+            raise ValueError('Cant find type name')
+
+    def __getShaFromEvent(self, boxEvent):
+        source = self.__getSource(boxEvent)
+        sha = source.get('sha1')
+        if(sha):
+            return sha
+        else:
+            return 0   #sha is 0 for folders so no throwing exception here
+
+    def __getFullPathFromEvent(self, boxEvent):
+        source = self.__getSource(boxEvent)
+        filename = source.get('name')
+        if(not filename):
+            raise ValueError('Cant find name')  
+        pathCollection = source.get('path_collection')
+        if(not pathCollection):
+            raise ValueError('Cant find path_collection') 
+        entries = pathCollection.get('entries')
+        if(not entries):
+            raise ValueError('Cant find entries')
+
+        fullpath = str()
+        for entry in entries:
+            name = entry['name']
+            if(name == 'All Files'):
+                name = '.'
+            fullpath = fullpath + name + "/"
+
+        fullpath = fullpath + filename
+        return fullpath
+
+
+    def __convertEvents(self, eventList):
+        convertedEvents = list()
+        for event in eventList:
+            try:
+                newEvent = Event()
+                newEvent.type = self.__getEventType(event)
+                newEvent.is_directory = (self.__getType(event) == 'folder')
+                newEvent.sha1 = self.__getShaFromEvent(event) 
+                newEvent.path = self.__getFullPathFromEvent(event)
+                convertedEvents.append(newEvent)
+            except ValueError as e:
+                print(e)
+        return convertedEvents
+
     def getLastEvents(self):
         eventsPack = self.events().get_events(limit=100, stream_position=self.lastEventStreamPosition)
         self.lastEventStreamPosition = eventsPack['next_stream_position']
-        return eventsPack['entries']
-
+        events = eventsPack['entries']
+        newEvents =  list()
+        ids = set([event['event_id'] for event in events])
+        for event in events:
+            if event['event_id'] not in self.ids:
+                newEvents.append(event)
+        self.ids = ids
+        return self.__convertEvents(newEvents)
 
 class Uploader:
 
@@ -54,21 +139,24 @@ class Downloader:
 
 def main():
     box = Box()
-    #while True:
-    #   for event in box.getLastEvents():
-    #       print(event)
-    #time.sleep(5)
+    while True:
+       for event in box.getLastEvents():
+           print("Type: " + str(event.type))
+           print("Is directory: " + str(event.is_directory))
+           print("Sha1: " + str(event.sha1))
+           print("Path: " + str(event.path))
+    time.sleep(5)
 
-    up = Uploader(box)
-    down  = Downloader(box)
+    # up = Uploader(box)
+    # down  = Downloader(box)
 
-    try:
-        up.upload('README.md', 'README.md_uploaded')
-    except boxsdk.exception.BoxAPIException as e:
-        print("File You're trying to upload already exists remotely")
-        print(e)
+    # try:
+    #     up.upload('README.md', 'README.md_uploaded')
+    # except boxsdk.exception.BoxAPIException as e:
+    #     print("File You're trying to upload already exists remotely")
+    #     print(e)
 
-    down.download('README.md_uploaded', 'README.md_downloaded')
+    # down.download('README.md_uploaded', 'README.md_downloaded')
 
     os._exit(0)
 
--- remote.py
+++ remote.py
@@ -7,7 +7,6 @@ import threading
 import time
 from box import Box
 
-
 class Observer(threading.Thread):
 
     def __init__(self, box, updateTime = 6):
@@ -29,27 +28,8 @@ class Observer(threading.Thread):
         self.isStopped = True
         self.join()
 
-    def getEvents(self):
-        events = box.getLastEvents()
-        newEvents =  list()
-        ids = set([event['event_id'] for event in events])
-        for event in events:
-            if event['event_id'] not in self.ids:
-                newEvents.append(event)
-        self.ids = ids
-        return newEvents
-
     def processEvent(self, event):
         pass
-
-
-class Event:
-
-    def __init__(self):
-        self.is_directory = bool()
-        self.src_path = str()
-        self.dest_path = str()
-
 
 class Handler:
 
@@ -67,48 +47,6 @@ class Handler:
 
     def on_modified(self, event):
         pass
-
-    def on_moved(self, event):
-        pass
-
-
-
-
-'''
-def get_events(client):
-    print(client.events().get_events(limit=100, stream_position='now'))
-
-def get_latest_stream_position(client):
-    print(client.events().get_latest_stream_position())
-
-def run_examples(oauth):
-    client = Client(oauth)
-    get_events(client)
-    get_latest_stream_position(client)
-
-    #routine below should be run on different thread cause it's blocking!
-    for event in client.events().generate_events_with_long_polling():
-        print('------------------------------------------------')
-        print('New notification arrived!')
-        print('Event type ' + event.get('event_type',''))
-        print('File hash ' + event.get('source').get('sha1',''))
-        print('File name ' + event.get('source').get('name',''))
-        print('File type ' + event.get('source').get('type',''))
-
-        absolutePath = '';
-
-        for directory in event.get('source').get('path_collection').get('entries'):
-            absolutePath = absolutePath +'/'+ directory.get('name')
-
-        print('Path ' + '"' + absolutePath + '"')
-        print('All info below:')
-        print(event)
-        print('------------------------------------------------')
-def main():
-    oauth = authenticate()
-    run_examples(oauth)
-    os._exit(0)
-'''
 
 if __name__ == '__main__':
     box = Box()

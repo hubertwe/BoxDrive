cea1867b86d0a7d990d8b2a6b2b389cbdfa4f10a
radx64 <radekteam@gmail.com>
2015-05-07 16:07:55 +0200

    Local and Remote SHA1 indexer class

--- indexer.py
+++ indexer.py
@@ -4,10 +4,31 @@ from box import Box
 import hashlib
 from glob import glob
 
+class FileIndex():
+    def __init__(self, filename, path, sha1):
+        self.filename = filename
+        self.path = path
+        self.sha1 = sha1
+    def getName(self):
+        return self.filename
+
+    def getPath(self):
+        return self.path
+
+    def getSha(self):
+        return self.sha1
+
 class Indexer:
     
     def __init__(self):
-        pass
+        self.remoteFiles = list()
+        self.localFiles = list()
+
+    def getRemoteIndex(self):
+        return self.remoteFiles
+
+    def getLocalIndex(self):
+        return self.localFiles
 
     def __sha(self, path):
         hasher = hashlib.sha1()
@@ -20,33 +41,35 @@ class Indexer:
         return hasher.hexdigest()
 
     def local(self, directoryPath):
-        result = dict()
+        print("Local indexing started...")
         for root, dirnames, filenames in os.walk(directoryPath):
             for filename in filenames:
                 fullFilename = os.path.join(root,filename) 
-                result[fullFilename] = self.__sha(fullFilename)
-        
-        for key, element in result.items():
-            print(str(element) + " - " + str(key))
-        pass
+                self.localFiles.append(FileIndex(filename, root, self.__sha(fullFilename)))
+        print("Local indexing finished.")
 
     def __processDirectory(self, directoryObject):
-        items = directoryObject.get_items(limit=1000)
+        items = directoryObject.get_items(limit=10000)
+        currentPath = "";
         for item in items:
-            print(item)
             if type(item) is boxsdk.object.folder.Folder:
-                print("Running resursively")
                 self.__processDirectory(item)
+            else:
+                fileInfo = item.get()  
+                for element in fileInfo['path_collection']['entries']:
+                    if element['id'] == '0':
+                        element['name'] = '.'
+                    currentPath += element['name'] + '/'
+                self.remoteFiles.append(FileIndex(fileInfo['name'],currentPath,fileInfo['sha1']))
 
-    def remote(self, box, directoryPath):
+    def remote(self, box):
+        print("Remote indexing started...")
         root = box.getRoot()
-        items = root.get_items(limit = 1000)
-        for fileObject in items:
-            print(fileObject)
-
+        self.__processDirectory(root)
+        print("Remote indexing finished.")
 
 if __name__ == '__main__':
     index = Indexer();
     index.local('.');
     box = Box();
-    index.remote(box,'any')
+    index.remote(box);
